52f4e24d3676fa3c6f677188d540530c092d84a1
GeraldR63 <Oracle.Beratung@yahoo.de>
2023-02-21 17:37:23 +0100

    1

--- app/src/main/java/com/nav/astronavigator/DialogBox.java
+++ app/src/main/java/com/nav/astronavigator/DialogBox.java
@@ -17,19 +17,21 @@ public class DialogBox
     }
     void show(String title, String message)
     {
+        try {
+            dialog = new AlertDialog.Builder(this.view.getContext()) // Pass a reference to your main activity here
+                    .setTitle(title)
+                    .setMessage(message)
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialog.cancel();
+                        }
+                    })
+                    .show();
+        } catch (Exception e)
+        {
 
-        dialog = new AlertDialog.Builder(this.view.getContext()) // Pass a reference to your main activity here
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener()
-                {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i)
-                    {
-                        dialog.cancel();
-                    }
-                })
-                .show();
+        }
     }
 
 
